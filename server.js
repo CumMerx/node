8f04d3aa8c41fca4aa6d7cfec339e17016209dbf
CumMerx <103858235+CumMerx@users.noreply.github.com>
2022-11-02 12:23:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => res.send("Hola Mundo  soy muy bueno"))
+app.get('/', (req, res) => res.send("Hola Mundo  angie y carlos))
 
 app.listen(3000);
 console.log("Server on port 3000")
